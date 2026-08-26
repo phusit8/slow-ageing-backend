8b817a85f1604f970a55dbdce46448b9eaf5cfa7
phusitrubun <158720692+phusitrubun@users.noreply.github.com>
2026-08-26 13:36:31 +0700

    done

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const LIFF_ID = process.env.LIFF_ID || "YOUR_LIFF_ID_HERE";
 
+//"YOUR_LIFF_ID_HERE"
+
 // Middleware
 app.use(cors());
 app.use(express.json());
